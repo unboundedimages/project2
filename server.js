c26ab38df785e6efd6debd50c7acf9555dbb7366
Teddy Bermes <30354478+bermessa@users.noreply.github.com>
2017-12-20 14:37:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+
 // Sets up the Express app to handle data parsing
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static("public"));
+
 
 // Sets up Express Engine Handlebars
 var exphbs = require("express-handlebars");
@@ -19,12 +21,13 @@ app.set("view engine", "handlebars");
 
 // Routes
 // =============================================================
-require('./routes/html-routes.js')(app);
+var routes = require("./controllers/tripController.js");
+app.use("/login", routes);
+
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 var db = require("./models");
-
 db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
