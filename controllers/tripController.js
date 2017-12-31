03945700367a25570d3488b48b616670c4312b59
Teddy Bermes <bermessa@gmail.com>
2017-12-31 02:28:27 +0000

    working axios!

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -34,25 +34,75 @@ router.get("/results", function(req, res) {
 });
 
 router.post("/api/newSearch", function(req, res) {
-    console.log("this is working(ish)");
-    // reassign variable here
-    axios({
-        method: 'get',
-        url: 'https://api.wunderground.com/api/' + authKey + '/history_' + req.body.date + '/q/' + req.body.state + '/' + req.body.city + '.json'
-    }).then(function(axiosResults) {
-        console.log(axiosResults.data.history.observations[5].tempi);
-        db.SearchLocation.create({
-            date: req.body.date,
-            state: req.body.state,
-            city: req.body.city,
-            // precipitation: axiosResults.data.history.observations[10].conds,
-            precipitation: axiosResults.data.history.observations[10].rain,
-            temperature: axiosResults.data.history.observations[10].tempi,
-            humidity: axiosResults.data.history.observations[10].hum
-        }).then(function(databaseResult) {
-            res.json(databaseResult);
-        });
-    });
+    console.log(req.body.date);
+
+    function getYear2005() {
+        return axios.get('https://api.wunderground.com/api/' + authKey + '/history_2005' + req.body.date + '/q/' + req.body.state + '/' + req.body.city + '.json');
+    }
+
+    function getYear2010() {
+        return axios.get('https://api.wunderground.com/api/' + authKey + '/history_2010' + req.body.date + '/q/' + req.body.state + '/' + req.body.city + '.json');
+    }
+
+    function getYear2015() {
+        return axios.get('https://api.wunderground.com/api/' + authKey + '/history_2015' + req.body.date + '/q/' + req.body.state + '/' + req.body.city + '.json');
+    }
+
+    function getImage() {
+        return axios.get('https://api.wunderground.com/api/' + authKey + '/satellite/q/' + req.body.state + '/' + req.body.city + '.json');
+    }
+
+    function getForecast() {
+        return axios.get('http://api.wunderground.com/api/' + authKey + '/forecast/q/' + req.body.state + '/' + req.body.city + '.json');
+    }
+
+
+    axios.all([getYear2005(), getYear2010(), getYear2015(), getImage(), getForecast()])
+        .then(axios.spread(function(year2005, year2010, year2015, image, forecast) {
+
+            db.SearchLocation.create({
+                date: req.body.date,
+                state: req.body.state,
+                city: req.body.city,
+                conds05: year2005.data.history.observations[10].conds,
+                conds10: year2010.data.history.observations[10].conds,
+                conds15: year2015.data.history.observations[10].conds,
+                temp05: year2005.data.history.observations[10].tempi,
+                temp10: year2010.data.history.observations[10].tempi,
+                temp15: year2015.data.history.observations[10].tempi,
+                hum05: year2005.data.history.observations[10].hum,
+                hum10: year2005.data.history.observations[10].hum,
+                hum15: year2005.data.history.observations[10].hum,
+                image: image.data.satellite.image_url,
+                todayDate: forecast.data.forecast.simpleforecast.forecastday[0].date.pretty,
+                todayHigh: forecast.data.forecast.simpleforecast.forecastday[0].high.fahrenheit,
+                todayLow: forecast.data.forecast.simpleforecast.forecastday[0].low.fahrenheit,
+                todayConds: forecast.data.forecast.simpleforecast.forecastday[0].conditions
+            }).then(function(databaseResult) {
+                res.json(databaseResult);
+            });
+        }));
+
+
+    // console.log("this is working(ish)");
+    // // reassign variable here
+    // axios({
+    //     method: 'get',
+    //     url: 'https://api.wunderground.com/api/' + authKey + '/history_' + req.body.date + '/q/' + req.body.state + '/' + req.body.city + '.json'
+    // }).then(function(axiosResults) {
+    //     console.log(axiosResults.data.history.observations[5].tempi);
+    //     db.SearchLocation.create({
+    //         date: req.body.date,
+    //         state: req.body.state,
+    //         city: req.body.city,
+    //         // precipitation: axiosResults.data.history.observations[10].conds,
+    //         precipitation: axiosResults.data.history.observations[10].rain,
+    //         temperature: axiosResults.data.history.observations[10].tempi,
+    //         humidity: axiosResults.data.history.observations[10].hum
+    //     }).then(function(databaseResult) {
+    //         res.json(databaseResult);
+    //     });
+    // });
 
     // axios({
     //     method: 'get',
--- models/searchLocation.js
+++ models/searchLocation.js
@@ -21,18 +21,48 @@ module.exports = function(sequelize, DataTypes) {
                 len: [1]
             }
         },
-        precipitation: {
+        conds05: {
             type: DataTypes.STRING
         },
-        temperature: {
+        temp05: {
             type: DataTypes.STRING
         },
-        humidity: {
+        hum05: {
             type: DataTypes.STRING
         },
-        // image: {
-        //     type: DataTypes.STRING
-        // }
+        conds10: {
+            type: DataTypes.STRING
+        },
+        temp10: {
+            type: DataTypes.STRING
+        },
+        hum10: {
+            type: DataTypes.STRING
+        },
+        conds15: {
+            type: DataTypes.STRING
+        },
+        temp15: {
+            type: DataTypes.STRING
+        },
+        hum15: {
+            type: DataTypes.STRING
+        },
+        image: {
+            type: DataTypes.STRING
+        },
+        todayDate: {
+            type: DataTypes.STRING
+        },
+        todayHigh: {
+            type: DataTypes.STRING
+        },
+        todayLow: {
+            type: DataTypes.STRING
+        },
+        todayConds: {
+            type: DataTypes.STRING
+        }
     });
     return SearchLocation;
 };
